0c755f7659eff15f59361695a5b56cd15f1343d7
eduardo-salazar <esalazar922@hotmail.com>
2013-03-18 01:37:24 -0600

    Version 1.0.0.1 que fue lanzada al store

---
+++ MisTareas/js/default.js
@@ -0,0 +1,149 @@
+﻿///<reference path="/MobileServicesJavaScriptClient/MobileServices.js" />
+// For an introduction to the Navigation template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232506
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+    var taskList = new WinJS.Binding.List();
+    var commentList = new WinJS.Binding.List();
+    var client = new Microsoft.WindowsAzure.MobileServices.MobileServiceClient(
+        "https://todaytasks.azure-mobile.net/",
+        "gYuUtqYiFziFetiPqclVzYwhXGmUwT64"
+    );
+    //--------------facebook------------------
+    var userId = null;
+    //---------------Notification Channel-------
+
+    var channel;
+
+
+    
+    //--------------facebook------------------
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            var publicMembers = { TaskList: taskList,Client:client,CommentList:commentList,Channel:channel };
+            WinJS.Namespace.define("Datos", publicMembers);
+
+            //facebook
+            // Request authentication from Mobile Services using a Facebook login.
+            var login = function () {
+                return new WinJS.Promise(function (complete) {
+                    client.login("facebook").done(function (results) {
+                        userId = results.userId;
+                        var message = "You are logged with your Facebook account: ";
+                        var dialog = new Windows.UI.Popups.MessageDialog(message);
+                        dialog.showAsync().done(complete);
+                        //cargar task del cloud
+                        loadTasks();
+                        //cargar comments del cloud
+                        loadComments();
+
+                    }, function (error) {
+                        userId = null;
+                        var dialog = new Windows.UI.Popups
+                            .MessageDialog("There is a problem whith the service", "Login Required");
+                        dialog.showAsync().done(complete);
+                    });
+                });
+            }
+
+
+            var authenticate = function () {
+                login().then(function () {
+                    if (userId === null) {
+
+
+                        // Authentication failed, try again.
+                        authenticate();
+                    }
+                });
+
+
+            }
+
+
+            authenticate();
+
+            //fin fb
+           
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                //Push Notification
+                //Get the channel for the application
+                var channelOperation = Windows.Networking.PushNotifications.PushNotificationChannelManager
+                    .createPushNotificationChannelForApplicationAsync().then(function (newChannel) {
+                        Datos.Channel = newChannel;
+                    });
+
+
+                //Agregar evento que escucha cuando se agrega un item a la list
+                taskList.addEventListener("iteminserted", addImage, false);
+                taskList.addEventListener("itemchanged",addImage,false);
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
+    function loadComments() {
+        var commentsTable = client.getTable('Comments').where({usuario:userId});
+        var todoItems;
+        commentsTable.read().done(function (results) {
+            todoItems = new WinJS.Binding.List(results);
+            todoItems.forEach(function (item) { Datos.CommentList.push(item); });
+        });
+    }
+
+    function loadTasks() {
+        
+        var tasksTable = client.getTable('Tasks').where({usuario:userId});
+        var todoItems;
+        tasksTable.read().done(function (results) {
+            todoItems = new WinJS.Binding.List(results);
+            todoItems.forEach(function (item) { Datos.TaskList.push(item); });
+        });
+    }
+     function addImage(args) {
+        var item = args.detail.value;
+        if (taskList.getAt(args.detail.index).realizada == false) {
+            var a=taskList.getAt(args.detail.index);
+            a["imagen"]="img/checks/uncheck.png";
+        }
+        else {
+            var a=taskList.getAt(args.detail.index);
+            a["imagen"]="img/checks/check.png";
+        }
+    }
+
+    
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
+    };
+    app.onsettings = function (e) {
+        e.detail.applicationcommands = { "about": { title: "About ", href: "/about.html" } };
+        WinJS.UI.SettingsFlyout.populateSettings(e);
+    };
+    app.start();
+})();
---
+++ MisTareas/pages/home/home.js
@@ -0,0 +1,207 @@
+﻿(function () {
+    "use strict";
+    var target = "";
+    
+    function showAppBar(currentItem) {
+        // Get the app bar.
+        var element = document.activeElement;
+        var appbar = document.getElementById("appbar");
+
+        // Keep the app bar open after it's shown.
+        appbar.winControl.sticky = true;
+
+        // Set the app bar context.
+        showItemCommands();
+
+        // Show the app bar.
+        appbar.winControl.show();
+
+        // Return focus to the original item which invoked the app bar.
+        if (element != null) element.focus();
+    }
+
+    function hideAppBar() {
+        var element = document.activeElement;
+        var appbar = document.getElementById("appbar");
+        appbar.winControl.sticky = false;
+        appbar.winControl.hide();
+        hideItemCommands();
+        if (element != null) element.focus();
+    }
+    function showItemCommands() {
+        appbar.winControl.showCommands([cmdDelete]);
+    }
+
+    function hideItemCommands() {
+        appbar.winControl.hideCommands([cmdDelete]);
+    }
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            
+            //Agregar tarea
+            var btnAdd = document.getElementById("addButton");
+            btnAdd.addEventListener("click", this.addTask, false);
+            //cambiar estado de tarea
+            var btncheck = document.getElementById("taskList");
+            btncheck.addEventListener("click", this.changeState, false);
+            btncheck.addEventListener("iteminvoked", this.changeIndex, false);
+            btncheck.addEventListener("selectionchanged", this.showAppBarDelete, false);
+            //Evento save panel summary
+            var summary = document.getElementById("btnSave");
+            summary.addEventListener("click",this.updateTask,false);
+       
+            //Ocurtar boton delete del appbar
+            hideAppBar();
+            //handler del evento click de delete
+            var cmdDelete = document.getElementById("cmdDelete");
+            cmdDelete.addEventListener("click",this.deleteItems,false);
+
+            //handler para agregar comentario
+            var btnAddComment = document.getElementById("btnAddComment");
+            btnAddComment.addEventListener("click", this.addComment, false);
+            //handler para activar el appbar
+            var commentList = document.getElementById("commentsList");
+            commentList.addEventListener("selectionchanged", this.showAppBarDelete, false);
+
+            //facebook login
+        },
+        addComment: function (info) {
+            var comment =
+                {
+                    comentario: document.getElementById("commentInput").value,
+                    usuario: "",
+                    channel:Datos.Channel.uri
+                };
+            //Agregar al cloud
+            Datos.Client.getTable("Comments").insert(comment);
+            //Agregar a la lista
+            Datos.CommentList.push(comment);
+        },
+        deleteItems:function(info){
+            //Borrar items
+            var items = document.getElementById("taskList");
+            var comments = document.getElementById("commentsList");
+
+            //funcion para borrar del cloud
+            var borrarCloudTasks = function (value) {
+                Datos.Client.getTable("Tasks").del(Datos.TaskList.getAt(value));
+            };
+            var borrarListTasks = function (key) {
+                //borrar de la lista
+                var index = Datos.TaskList.indexOfKey(key);
+                Datos.TaskList.splice(index, 1);
+            };
+            if (items.winControl.selection.getItems()._value.length != 0) {                
+                var indexes = items.winControl.selection.getIndices();
+                indexes.forEach(borrarCloudTasks);
+                var keys=new WinJS.Binding.List();
+                indexes.forEach(function (index) {
+                    keys.push(Datos.TaskList.getItem(index).key);
+                });
+                
+                keys.forEach(borrarListTasks);
+            }
+            //funcion para borrar del cloud
+            var borrarCloudComments = function (value) {
+                Datos.Client.getTable("Comments").del(Datos.CommentList.getAt(value));
+            };
+            var borrarListComments = function (key) {
+                //borrar de la lista
+                var index = Datos.CommentList.indexOfKey(key);
+                Datos.CommentList.splice(index, 1);
+            };
+            if (comments.winControl.selection.getItems()._value.length != 0) {
+                var indexes = comments.winControl.selection.getIndices();
+                indexes.forEach(borrarCloudComments);
+                var keys = new WinJS.Binding.List();
+                indexes.forEach(function (index) {
+                    keys.push(Datos.CommentList.getItem(index).key);
+                });
+
+                keys.forEach(borrarListComments);
+            }
+            //if (comments.selection.getItems()._value.length != 0) {
+            //    var selectedComments = comments.selection.getItems();
+            //    var index2;
+            //    for (var j = 0; j < selectedComments._value.length; j++) {
+            //        //Borrar de la nube
+            //        index2 = selectedComments._value[j].index;
+            //        Datos.Client.getTable("Comments").del(Datos.CommentList.getAt(index2));
+
+            //        //borrar de la lista
+            //        Datos.CommentList.splice(index2, 1);
+            //    }
+            //}
+        },
+        showAppBarDelete: function (info) {
+            var btncheck = document.getElementById("taskList").winControl;
+            var comments = document.getElementById("commentsList").winControl;
+            //Visualizar appbar delete
+            if (btncheck.selection.count() == 0 && comments.selection.count()==0) {
+                hideAppBar();
+            }
+            else {
+                showAppBar();
+            }
+        },
+        updateTask:function(infoData){
+            //Obtener descripcion y fecha
+            var descripcion=document.getElementById("description").value;
+            var date=document.getElementById("date").winControl.current;
+            //Asignar valores al elemento seleccionado de la lista
+            Datos.TaskList.getAt(target).descripcion=descripcion;
+            Datos.TaskList.getAt(target).fecha=date.toDateString();
+            //Actualizar elemento en el cloud
+            Datos.Client.getTable("Tasks").update(Datos.TaskList.getAt(target));
+            //Notificar al listView que hay que actualizar
+            Datos.TaskList.notifyReload();
+        },
+        changeState: function (infoData) {
+           if (infoData.target.localName == "img") {
+                if (Datos.TaskList.getAt(target).realizada == false) {
+                    Datos.TaskList.getAt(target).realizada = true;
+                    Datos.TaskList.getAt(target).imagen = "img/checks/check.png";
+                }
+                else {
+                    Datos.TaskList.getAt(target).realizada = false;
+                    Datos.TaskList.getAt(target).imagen = "img/checks/uncheck.png";
+                }
+                Datos.TaskList.notifyReload();
+               //actualizar cloud
+                Datos.Client.getTable("Tasks").update(Datos.TaskList.getAt(target));
+            }
+        },
+        changeIndex: function (e) {
+            target = e.detail.itemIndex;
+
+            var description = document.getElementById("description");
+            var date = document.getElementById("date").winControl;
+            description.value = Datos.TaskList.getAt(target).descripcion;
+            date.current = Datos.TaskList.getAt(target).fecha;
+        }
+        ,
+        addTask: function (infoData) {
+            var fecha=new Date();
+            //Obtener titulo de la tarea
+            var input = document.getElementById("newTaskInput");
+            var task = 
+                {
+                    titulo: input.value,
+                    descripcion: "",
+                    fecha: fecha.toDateString(),
+                    usuario: "",
+                    realizada: false,
+                    channel:Datos.Channel.uri
+                };
+            //Agregar al windows azure
+            Datos.Client.getTable("Tasks").insert(task);
+            Datos.TaskList.push(task);
+
+            //limpiar input
+            input.value = "";
+        }
+    });
+})();
